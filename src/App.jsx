c6b35732f4a30656e60bd4a4326ea623e5b5bc45
AkhilaCheryala <akhilacheryala2804@gmail.com>
2024-04-06 20:29:39 +0530

    added alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
- import Nav from "./components/Nav";
- import Form from "./components/Form";
- import './App.css';
+import Nav from "./components/Nav";
+import Form from "./components/Form";
+import './App.css';
 import Enquiry from "./components/Enquiry";
 import Vector from "./components/Vector";
 import Footer from "./components/Footer";
@@ -8,30 +8,30 @@ import Prefooter from "./components/Prefooter";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Chatbox from "./components/Chatbox";
 function App() {
- 
+
 
   return (
-    <div className="bg-neutral-100">
-    <Nav/>
-    <div className=" lg:text-white  text-blue-900 lg:bg-blue-900 h-80 lg:h-96 md:h-60 p-2  pt-6 text-left  ">
-      <div className=" lg:pl-20 lg:pt-16 pl-5">
-      <p className="font-bold lg:w-80 text-3xl mt-3 md:text-4xl">Connect with our sales team.</p>
-      <p className="text-xl font-bold mt-4">Let’s talk about how PayPal can power your growth.</p>
-    </div>
-    </div>
-    <div >
-      <Form/>
+  <div className=" bg-neutral-100">
+      <Nav />
+      <div className=" lg:text-white  text-blue-900 lg:bg-blue-900 h-80 lg:h-96 md:h-60 p-2  pt-6 text-left  ">
+        <div className=" lg:pl-20 lg:pt-16 pl-5">
+          <p className="font-bold lg:w-80 text-3xl mt-3 md:text-4xl">Connect with our sales team.</p>
+          <p className="text-xl font-bold mt-4">Let’s talk about how PayPal can power your growth.</p>
+        </div>
       </div>
       <div >
-       <Enquiry/>
+        <Form />
       </div>
-      <Vector/>
-      <Prefooter/>
-     <Footer/>
-     <Chatbox/>
+      <div >
+        <Enquiry />
+      </div>
+      <Vector />
+      <Prefooter />
+      <Footer />
+      <Chatbox />
     </div>
-  
-  ) 
+
+  )
 }
 
 export default App
--- src/components/SignUpLogin.jsx
+++ src/components/SignUpLogin.jsx
@@ -11,47 +11,46 @@ const SignUpLogin = ({ isSignUp, onSignUp, onLogin, onClose }) => {
     } else {
       onLogin(username, password);
     }
+    if (username && password){ 
+    alert(`Welcome, ${username}!!`);
+    onClose();
+  }
     setUsername('');
     setPassword('');
   };
 
   const handleClose = () => {
-    onClose(); 
+    onClose();
   };
- const handleToggle =()=>{
-   if (isSignUp){
-    onLogin (username,password);
-   }else{
-    onSignUp(username,password);
-   }
-   setUsername('');
-   setPassword('');
-   console.log("toggle button is clicked");
-   console.log("isSignup:",isSignUp);
-   };
- 
+  const handleToggle = () => {
+    if (isSignUp) {
+      onLogin(username, password);
+    } else {
+      onSignUp(username, password);
+    }
+    setUsername('');
+    setPassword('');
+    console.log("toggle button is clicked");
+    console.log("isSignup:", isSignUp);
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
       <div className="bg-white p-8  rounded-md w-80">
-        <button onClick={handleClose}
-          className=" font-bold ml-[15rem] mt-[-1rem] text-red-700 text-xl "> X </button>
+        <button onClick={handleClose} className=" font-bold ml-[15rem] mt-[-1rem]  text-red-600 text-xl "> X </button>
         {isSignUp ? (
           <div>
             <h2 className="text-2xl font-bold mb-6 underline text-blue-600 text-center">Sign Up</h2>
             <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-              <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="border p-2 rounded-md"/>
+              <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}
+                className="border p-2 rounded-md" required />
               <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}
-                className="border p-2 rounded-md"/>
+                className="border p-2 rounded-md" required/>
               <button type="submit" className="bg-blue-500 text-white py-2 rounded-md hover:bg-blue-300">
-                 Sign Up
+                Sign Up
               </button>
               <button onClick={handleToggle}>
-                Already have an account? <span  className="text-blue-500 underline mt-2">Login</span>
+                Already have an account? <span className="text-blue-500 underline mt-2">Login</span>
               </button>
             </form>
           </div>
@@ -59,28 +58,15 @@ const SignUpLogin = ({ isSignUp, onSignUp, onLogin, onClose }) => {
           <div>
             <h2 className="text-3xl font-bold underline  mb-6 text-center text-blue-600">Login</h2>
             <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-              <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="border p-2 rounded-md"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="border p-2 rounded-md"
-              />
-              <button
-                type="submit"
-                className="bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-              >
+              <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}
+                className="border p-2 rounded-md" required/>
+              <input  type="password"  placeholder="Password"  value={password} onChange={(e) => setPassword(e.target.value)}
+                className="border p-2 rounded-md" required />
+              <button type="submit" className="bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600" >
                 Login
               </button>
               <button onClick={handleToggle}>
-                Don't have an account? <span className="text-blue-500 mt-2 underline">Sign Up</span> 
+                Don't have an account? <span className="text-blue-500 mt-2 underline">Sign Up</span>
               </button>
             </form>
           </div>
